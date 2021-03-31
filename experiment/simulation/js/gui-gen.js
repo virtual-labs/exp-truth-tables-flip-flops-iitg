60c08d30eba1554812ceae0fc905ce2ada1f3b85
IITGVLAB <75976828+IITGVLAB@users.noreply.github.com>
2021-03-31 11:25:27 +0530

    Update gui-gen.js

--- experiment/simulation/js/gui-gen.js
+++ experiment/simulation/js/gui-gen.js
@@ -18,7 +18,7 @@ $.fx.speeds._default = 1000;
 var setUI = function(){
     $( "#componentsDialog" ).dialog({
         autoOpen: false,
-        position: ['left','center']
+        position: ['left+200','top+200']
     });
 	
 	//$( "#componentsDialog" ).dialog( "option", "maxHeight", 400 );
@@ -224,6 +224,6 @@ var retriveConnectionsFromDB = function(expId){
 
 $(document).ready(function() {
     //$('#cktBody').css({'height':$(window).height() - 90});
-    $('#sideBar').css({'height':$(window).height() - 90});
+//     $('#sideBar').css({'height':$(window).height() - 90});
     setUI();
 });
